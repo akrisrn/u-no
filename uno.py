f9a451375fa79bbd7b7a3587f51393446c2233bf
AkrISrn <shairkarine@gmail.com>
2018-03-19 21:37:58 +0800

    fix style

--- uno.py
+++ uno.py
@@ -80,8 +80,8 @@ def article(dir_name, file_sha1):
         tags = []
         md_tags = group.group(2)[2:].split(", ")
         for md_tag in md_tags:
-            group = re.search("\[(.*?)\]\((.*?)\)", md_tag)
-            tags.append([group.group(1), group.group(2)])
+            group = re.search("\[(.*?)\]\(.*?\)", md_tag)
+            tags.append(group.group(1))
         return render_template('article.html', name=name, content=content, tags=tags, show_tags=True)
     else:
         file_dir, file = os.path.split(os.path.join(articles_dir_abspath, file_path))
