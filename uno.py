f6ade65ea3f6ff3209318d0cd3465735bdea39f2
AkrISrn <shairkarine@gmail.com>
2018-03-30 17:40:21 +0800

    small fix

--- uno.py
+++ uno.py
@@ -13,12 +13,12 @@ app.register_blueprint(main)
 app.jinja_env.auto_reload = True
 # 绑定变量到jinja模板
 app.jinja_env.globals["site_name"] = uno_site_name
-app.jinja_env.globals["index_id_name"] = index_id_key
-app.jinja_env.globals["index_title_name"] = index_title_key
-app.jinja_env.globals["index_url_name"] = index_url_key
-app.jinja_env.globals["index_date_name"] = index_date_key
-app.jinja_env.globals["index_tags_name"] = index_tags_key
-app.jinja_env.globals["index_notags_name"] = index_notags_key
+app.jinja_env.globals["index_id_key"] = index_id_key
+app.jinja_env.globals["index_title_key"] = index_title_key
+app.jinja_env.globals["index_url_key"] = index_url_key
+app.jinja_env.globals["index_date_key"] = index_date_key
+app.jinja_env.globals["index_tags_key"] = index_tags_key
+app.jinja_env.globals["index_notags_key"] = index_notags_key
 # 绑定函数到jinja模板
 app.jinja_env.globals.update(get_static_file_url=get_static_file_url)
 app.jinja_env.globals.update(get_static_lib_url=get_static_lib_url)
