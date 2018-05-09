cb1db93d5cf8aad3ddc569d7d774e5eacb5e986a
AkrISrn <shairkarine@gmail.com>
2018-05-09 13:18:16 +0800

    fix index

--- uno.py
+++ uno.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 from config import uno_debug, uno_port, uno_host, uno_site_name, uno_static_dir_name, uno_secret_key
 from src.index import index_url_key, index_title_key, index_id_key, index_tags_key, index_date_key, index_notags_key, \
-    index_highlight_key
+    index_highlight_key, index_fixed_key, index_top_key
 from src.util import get_static_file_url, get_static_lib_url
 from src.view.main import main
 
@@ -21,6 +21,8 @@ app.jinja_env.globals["index_date_key"] = index_date_key
 app.jinja_env.globals["index_tags_key"] = index_tags_key
 app.jinja_env.globals["index_notags_key"] = index_notags_key
 app.jinja_env.globals["index_highlight_key"] = index_highlight_key
+app.jinja_env.globals["index_fixed_key"] = index_fixed_key
+app.jinja_env.globals["index_top_key"] = index_top_key
 # 绑定函数到jinja模板
 app.jinja_env.globals.update(get_static_file_url=get_static_file_url)
 app.jinja_env.globals.update(get_static_lib_url=get_static_lib_url)
