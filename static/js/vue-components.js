6ceb9ad768c1e2d77f2a67541ac09e9bb91ee437
AkrISrn <shairkarine@gmail.com>
2018-06-17 16:56:09 +0800

    add vue-tag component

--- static/js/vue-components.js
+++ static/js/vue-components.js
@@ -1,5 +1,18 @@
 Vue.component('vue-return-home', {
-    template: '<a href="/"><i style="margin-right: 10px" class="fas fa-angle-double-left"></i>HOME\n</a>'
+    template: `
+<a href="/">
+    <i style="margin-right: 10px" class="fas fa-angle-double-left"></i>HOME
+</a>`
+});
+
+Vue.component('vue-tag', {
+    props: ['url', 'name'],
+    template: `
+<div class="tag">
+    <a href="{{ url }}">
+        <i style="margin-right: 3px" class="fas fa-hashtag"></i>{{ name }}
+    </a>
+</div>`
 });
 
 new Vue({el: '#content'});
