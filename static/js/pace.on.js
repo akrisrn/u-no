24ab07131590492b523cf729184c88642a25bf93
AkrISrn <shairkarine@gmail.com>
2018-06-17 23:17:40 +0800

    small fix

--- static/js/pace.on.js
+++ static/js/pace.on.js
@@ -2,8 +2,3 @@ Pace.on("done", function () {
     document.getElementById("content").style.opacity = "1";
     document.getElementById("content").style.top = "0";
 });
-
-window.onbeforeunload = function () {
-    document.getElementById("content").style.opacity = "0";
-    document.getElementById("content").style.top = "30px";
-};
