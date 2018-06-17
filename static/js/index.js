8f95edc52fa3c1d921235ca34d8e6afb99f5e3ad
AkrISrn <shairkarine@gmail.com>
2018-06-17 21:50:53 +0800

    refactor

--- static/js/index.js
+++ static/js/index.js
@@ -1,63 +1,58 @@
 $(document).ready(function () {
-    function GetRequest() {
-        const url = location.search;
-        const request = {};
+    let inputs = [$("#idS"), $("#titleS"), $("#tagS"), $("#dateS")];
+    let flags = ["i", "n", "t", "d"];
+
+    inputs.forEach(function (item) {
+        item.keydown(function () {
+            if (event.keyCode === 13) search.click()
+        });
+    });
+
+    let request = function () {
+        let url = location.search;
+        let request = {};
         if (url.indexOf("?") !== -1) {
-            const str = url.substr(1);
-            const strList = str.split("&");
+            let str = url.substr(1);
+            let strList = str.split("&");
             for (let i = 0; i < strList.length; i++) {
                 request[strList[i].split("=")[0]] = decodeURI(strList[i].split("=")[1]);
             }
         }
         return request;
-    }
+    }();
 
-    const id = $("#idS");
-    const title = $("#titleS");
-    const tag = $("#tagS");
-    const date = $("#dateS");
-
-    const request = GetRequest();
-    id.val(request["i"]);
-    title.val(request["n"]);
-    tag.val(request["t"]);
-    date.val(request["d"]);
+    inputs.forEach(function (item, index) {
+        item.val(request[flags[index]]);
+    });
 
     $("#search").click(function () {
-        const idVal = $.trim(id.val());
-        const nameVal = $.trim(title.val());
-        const tagVal = $.trim(tag.val());
-        const dateVal = $.trim(date.val());
+        let values = [];
+        inputs.forEach(function (item) {
+            values.push($.trim(item.val()))
+        });
+
         let href = "";
-        if (idVal !== "") {
-            href += "&i=" + idVal
-        }
-        if (nameVal !== "") {
-            href += "&n=" + nameVal
-        }
-        if (tagVal !== "") {
-            href += "&t=" + tagVal
-        }
-        if (dateVal !== "") {
-            href += "&d=" + dateVal
-        }
-        if (href !== "") {
-            href = "?" + href.substr(1)
-        }
+
+        values.forEach(function (item, index) {
+            if (item !== "") href += "&" + flags[index] + "=" + item
+        });
+
+        if (href !== "") href = "?" + href.substr(1);
+
         window.location.href = window.location.pathname + href
     });
 
     $("#clean").click(function () {
-        id.val("");
-        title.val("");
-        tag.val("");
-        date.val("");
+        inputs.forEach(function (item) {
+            item.val("");
+        });
     });
 
-    document.getElementById("defaultOpen").click();
+    let defaultOpen = document.getElementById("defaultOpen");
+    if (defaultOpen !== null) defaultOpen.click()
 });
 
-function openCity(evt, cityName, t) {
+function openTab(evt, tabName, t) {
     let i, tabcontent, tablinks;
 
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -71,7 +66,7 @@ function openCity(evt, cityName, t) {
         tablinks[i].children[0].className = tablinks[i].children[0].className.replace("-open", "");
     }
 
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
     t.children[0].className = t.children[0].className + "-open"
 }
