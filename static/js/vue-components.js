f06b484dfb044548d8dae53ce7d97f5d2284d2e2
AkrISrn <shairkarine@gmail.com>
2018-06-17 18:15:13 +0800

    add some components

--- static/js/vue-components.js
+++ static/js/vue-components.js
@@ -9,10 +9,32 @@ Vue.component('vue-tag', {
     props: ['url', 'name'],
     template: `
 <div class="tag">
-    <a href="{{ url }}">
+    <a :href="url">
         <i style="margin-right: 3px" class="fas fa-hashtag"></i>{{ name }}
     </a>
 </div>`
+});
+
+Vue.component('vue-date', {
+    props: ['date'],
+    template: '<div class="date">{{ date }}</div>'
+});
+
+Vue.component('vue-tag-icon', {
+    template: '<i class="fas fa-tags fa-xs"></i>'
+});
+
+Vue.component('vue-home-li-icon', {
+    template: '<i class="fas fa-angle-double-right"></i>'
+});
+
+Vue.component('vue-home-li', {
+    props: ['url', 'name', 'highlight'],
+    template: `
+<a :href="url" :class="{ 'article-hl': highlight }">
+    <i class="fas fa-star" v-if="highlight"></i>
+    {{ name }}
+</a>`
 });
 
 new Vue({el: '#content'});
