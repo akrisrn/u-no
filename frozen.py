74393465765c237fb0339e3390ab03722e2f6e68
AkrISrn <shairkarine@gmail.com>
2018-10-15 15:03:00 +0800

    fix

--- frozen.py
+++ frozen.py
@@ -9,21 +9,11 @@ from src.index import index_url_key, index_tags_key, index_title_key, get_item_b
 from src.view.main import index, article_page, tag_page
 from uno import app
 
-uno_tags_url_name = "tags"
-
-
-def replace_url(data, urls):
-    for result in re.finditer("/(%s|%s|%s)/[0-9a-z]{40}" %
-                              (uno_articles_url_name, uno_attachments_url_name, uno_tags_url_name), data):
-        data = re.sub(result.group(), urls[result.group()], data)
-    return data.replace("http:///", "/")
-
-
 if __name__ == '__main__':
+    uno_tags_url_name = "tags"
     app.config["SERVER_NAME"] = ""
 
     root_abspath = src.util.get_root_abspath()
-
     static_dir_abspath = os.path.join(root_abspath, uno_static_dir_name)
 
     frozen_dir_abspath = os.path.join(root_abspath, uno_frozen_dir_name)
@@ -46,10 +36,9 @@ if __name__ == '__main__':
     shutil.copytree(static_dir_abspath, frozen_static_dir_abspath,
                     ignore=shutil.ignore_patterns("bower_components", "node_modules", "*.json"))
 
+    data_list = {}
+    page_urls = {}
     with app.app_context():
-        data_list = {}
-        page_urls = {}
-
         index_page_data = index()
         data_list[os.path.join(frozen_dir_abspath, "index.html")] = index_page_data
         for result_article in re.finditer("/%s/[0-9a-z]{40}" % uno_articles_url_name, index_page_data):
@@ -75,6 +64,14 @@ if __name__ == '__main__':
                     data_list[tag_abspath] = tag_page(tag)
                     page_urls["/%s/%s" % (uno_tags_url_name, tag)] = "/%s/%s.html" % (uno_tags_url_name, tags[tag])
 
-        for path in data_list:
-            with open(path, "w", encoding="utf-8") as f:
-                f.write(replace_url(data_list[path], page_urls))
+
+    def replace_url(data, urls):
+        for result in re.finditer("/(%s|%s|%s)/[0-9a-z]{40}" %
+                                  (uno_articles_url_name, uno_attachments_url_name, uno_tags_url_name), data):
+            data = re.sub(result.group(), urls[result.group()], data)
+        return data.replace("http:///", "/")
+
+
+    for path in data_list:
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(replace_url(data_list[path], page_urls))
