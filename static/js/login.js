badf9888f3b675484bb3a7a4bff88d8a76a431a4
AkrISrn <shairkarine@gmail.com>
2018-10-15 09:31:37 +0800

    fix

--- static/js/login.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("input[type=password]").keydown(function (e) {
-        if (e.which === 13) {
-            $("button[type=submit]").click();
-            return false;
-        }
-    });
-});
