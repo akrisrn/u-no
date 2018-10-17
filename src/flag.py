9eec2c423daf843055fea04525a9c49587b9e57a
AkrISrn <shairkarine@gmail.com>
2018-10-17 09:08:37 +0800

    fix

--- src/flag.py
+++ src/flag.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from flask import current_app
 
 import index
+from const import flag_js, flag_css, flag_unignore, flag_ignore, flag_highlight, flag_top, flag_fixed, flag_notags, \
+    flag_tag, flag_date
 from util import clean_text, regexp_join
 
 
@@ -15,7 +17,7 @@ def get_flag_regexp(flag):
 # 获取文章里标记的标签列表，语法匹配<<tag()>>，如果没有则返回默认标签
 def get_tags_flag(data):
     default_tag = [current_app.config["DEFAULT_TAG"]]
-    group = re.search(get_flag_regexp("tag"), data)
+    group = re.search(get_flag_regexp(flag_tag), data)
     if not group:
         return default_tag
     # 生成标签列表
@@ -27,7 +29,7 @@ def get_tags_flag(data):
 
 # 获取文章里标记的日期，语法匹配<<date(%y-%m-%d)>>，如果没有则返回空
 def get_date_flag(data):
-    group = re.search(get_flag_regexp("date"), data)
+    group = re.search(get_flag_regexp(flag_date), data)
     if not group:
         return ""
     date = str(clean_text(group.group(1)).split(",")[0])
@@ -44,36 +46,36 @@ def get_date_flag(data):
 
 # 获取文章里标记的不展示标签标识，语法匹配<<notags()>>
 def get_notags_flag(data):
-    return True if re.search(get_flag_regexp("notags"), data) else False
+    return True if re.search(get_flag_regexp(flag_notags), data) else False
 
 
 # 获取文章里标记的固定索引标识，语法匹配<<fixed()>>
 def get_fixed_flag(data):
-    return True if re.search(get_flag_regexp("fixed"), data) else False
+    return True if re.search(get_flag_regexp(flag_fixed), data) else False
 
 
 # 获取文章里标记的置顶标识，语法匹配<<top()>>
 def get_top_flag(data):
-    return True if re.search(get_flag_regexp("top"), data) else False
+    return True if re.search(get_flag_regexp(flag_top), data) else False
 
 
 # 获取文章里标记的高亮标识，语法匹配<<highlight()>>
 def get_highlight_flag(data):
-    return True if re.search(get_flag_regexp("highlight"), data) else False
+    return True if re.search(get_flag_regexp(flag_highlight), data) else False
 
 
 # 获取文章里标记的忽略文件标识，语法匹配<<ignore()>>
 def get_ignore_flag(data):
-    return True if re.search(get_flag_regexp("ignore"), data) else False
+    return True if re.search(get_flag_regexp(flag_ignore), data) else False
 
 
 # 获取文章里标记的取消忽略文件标识，语法匹配<<unignore()>>
 def get_unignore_flag(data):
-    return True if re.search(get_flag_regexp("unignore"), data) else False
+    return True if re.search(get_flag_regexp(flag_unignore), data) else False
 
 
 # 获取文章里标记的自定义css文件列表，语法匹配<<css()>>，如果没有则返回空
-def get_custom_css_flag(data, custom_type="css"):
+def get_custom_css_flag(data, custom_type=flag_css):
     group = re.search(get_flag_regexp(custom_type), data)
     if not group:
         return []
@@ -89,4 +91,4 @@ def get_custom_css_flag(data, custom_type="css"):
 
 # 获取文章里标记的自定义js文件列表，语法匹配<<js()>>，如果没有则返回空
 def get_custom_js_flag(data):
-    return get_custom_css_flag(data, "js")
+    return get_custom_css_flag(data, flag_js)
