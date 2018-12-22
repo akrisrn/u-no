119ac6257b2378ab817f2d160d203827f34ec538
AkrISrn <shairkarine@gmail.com>
2018-12-22 11:50:40 +0800

    add PAGE_SIZE

---
+++ static/js/home.js
@@ -0,0 +1,47 @@
+let currentPageNum;
+
+$(function () {
+    function switchPage(num) {
+        $("ul > li").hide();
+        prevButton.hide();
+        nextButton.hide();
+
+        $(`li.page-${num}`).fadeIn();
+        if (num > 1) {
+            prevButton.show();
+        }
+        if (num < pageCount) {
+            nextButton.show();
+        }
+        for (let i = 0; i < pageCount; i++) {
+            const pageButton = pageButtons[i];
+            pageButton.classList.remove("hover");
+            if (i === num - 1) {
+                pageButton.classList.add("hover");
+            }
+        }
+        scrollTo(0, 0);
+        currentPageNum = num;
+    }
+
+    const buttonGroup = $(".button-group");
+    const pageCount = buttonGroup.children().length - 2;
+    const prevButton = buttonGroup.children().first();
+    const nextButton = buttonGroup.children().last();
+    const pageButtons = buttonGroup.children().slice(1, pageCount + 1);
+
+    switchPage(1);
+
+    for (let i = 0; i < pageCount; i++) {
+        const pageButton = pageButtons[i];
+        $(pageButton).click(() => {
+            switchPage(i + 1);
+        })
+    }
+    prevButton.click(() => {
+        switchPage(currentPageNum - 1);
+    });
+    nextButton.click(() => {
+        switchPage(currentPageNum + 1);
+    });
+});
--- uno.py
+++ uno.py
@@ -20,6 +20,7 @@ app.jinja_env.auto_reload = True
 # 绑定变量到jinja模板
 app.jinja_env.globals["site_name"] = app.config["SITE_NAME"]
 app.jinja_env.globals["default_tag"] = app.config["DEFAULT_TAG"]
+app.jinja_env.globals["page_size"] = app.config["PAGE_SIZE"]
 app.jinja_env.globals["index_id_key"] = index_id_key
 app.jinja_env.globals["index_title_key"] = index_title_key
 app.jinja_env.globals["index_path_key"] = index_path_key
