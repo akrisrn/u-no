c637c18ec2630170b921bb2b3543c35d6084032c
AkrISrn <shairkarine@gmail.com>
2018-12-24 11:34:04 +0800

    fix

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,10 +1,24 @@
 $(function () {
-    editormd({
+    const editor = editormd({
         id: "editormd",
         path: lib_path,
         height: "700px",
         watch: false
     });
+    const fullscreen = editor.fullscreen;
+    const fullscreenExit = editor.fullscreenExit;
+    editor.fullscreen = function () {
+        fullscreen.apply(editor);
+        if (editor.state.fullscreen) {
+            $(".button-group").hide();
+        }
+    };
+    editor.fullscreenExit = function () {
+        fullscreenExit.apply(editor);
+        if (!editor.state.fullscreen) {
+            $(".button-group").show();
+        }
+    };
 
     $("#save").click(() => {
         $.post(document.location.pathname, {data: $("#data").val()}, (result) => {
