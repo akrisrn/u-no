73e2fa53f54d38f9d94b199f672d4f8424bdf9b9
AkrISrn <shairkarine@gmail.com>
2018-12-25 16:31:06 +0800

    fix

--- static/js/edit.js
+++ static/js/edit.js
@@ -34,5 +34,14 @@ $(function () {
     });
     $("#view").click(() => {
         open(url)
-    })
+    });
+
+    $(window).bind("keydown", (event) => {
+        if (event.ctrlKey || event.metaKey) {
+            if (String.fromCharCode(event.which).toLowerCase() === "s") {
+                event.preventDefault();
+                $("#save").click()
+            }
+        }
+    });
 });
