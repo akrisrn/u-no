3037ca39e23ffc62f3bc228605bafd25a864246c
AkrISrn <shairkarine@gmail.com>
2019-01-06 12:42:59 +0800

    fix

--- static/js/vue-components.js
+++ static/js/vue-components.js
@@ -7,8 +7,7 @@ Vue.component('vue-return-home', {
             default: "~"
         }
     },
-    template: `
-<a v-bind:href="url"><i style="margin-right: 10px" class="fas fa-angle-double-left"></i>{{ name }}</a>`
+    template: `<a v-bind:href="url"><vue-return-home-icon style="margin-right: 10px"></vue-return-home-icon>{{ name }}</a>`
 });
 
 Vue.component('vue-tag', {
@@ -29,6 +28,29 @@ Vue.component('vue-date', {
     template: '<div class="date">{{ date }}</div>'
 });
 
+Vue.component('vue-home-li', {
+    props: {
+        url: String, name: String, is_highlight: {
+            type: Boolean,
+            default: false
+        }
+    },
+    template: `<a v-bind:href="url" v-bind:class="{'article-hl': is_highlight}"><vue-star-icon style="margin-right: 8px;" v-if="is_highlight"></vue-star-icon>{{ name }}</a>`
+});
+
+Vue.component('vue-error', {
+    props: ['title'],
+    template: '<div class="center"><h1>{{ title }}</h1></div>'
+});
+
+Vue.component('vue-index-tab-button', {
+    props: ['name', 'id'],
+    template: `
+<button class="tablinks" v-bind:id="id">
+    <vue-folder-icon style="margin-right: 8px"></vue-folder-icon>{{ name }}
+</button>`
+});
+
 Vue.component('vue-tag-icon', {
     template: '<i class="fas fa-tags fa-xs"></i>'
 });
@@ -41,38 +63,21 @@ Vue.component('vue-home-li-icon', {
     template: '<i class="fas fa-angle-double-right"></i>'
 });
 
+Vue.component('vue-return-home-icon', {
+    template: '<i class="fas fa-angle-double-left"></i>'
+});
+
 Vue.component('vue-star-icon', {
     template: '<i class="fas fa-star"></i>'
 });
 
-Vue.component('vue-home-li', {
-    props: {
-        url: String, name: String, is_highlight: {
-            type: Boolean,
-            default: false
-        }
-    },
-    template: `
-<a v-bind:href="url" v-bind:class="{'article-hl': is_highlight}"><vue-star-icon style="margin-right: 8px;" v-if="is_highlight"></vue-star-icon>{{ name }}</a>`
+Vue.component('vue-folder-icon', {
+    template: '<i class="far fa-folder"></i>'
 });
 
-Vue.component('vue-error', {
-    props: ['title'],
-    template: '<div class="center"><h1>{{ title }}</h1></div>'
-});
-
-Vue.component('vue-index-tab-button', {
-    props: ['name', 'id'],
-    template: `
-<button class="tablinks" v-bind:id="id">
-    <i style="margin-right: 8px" class="far fa-folder"></i>{{ name }}
-</button>`
-});
-
-Vue.component('vue-check', {
+Vue.component('vue-check-icon', {
     props: ['checked'],
-    template: `
-<i v-bind:class="['fas', checked ? 'fa-check' : 'fa-times', 'check']"></i>`
+    template: `<i v-bind:class="['fas', checked ? 'fa-check' : 'fa-times', 'check']"></i>`
 });
 
 new Vue({
