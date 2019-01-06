699cac9685f12dc31cfabcfa8c12c13889f4c4ca
AkrISrn <shairkarine@gmail.com>
2019-01-06 18:35:14 +0800

    fix

---
+++ static/js/tags.js
@@ -0,0 +1,11 @@
+$(function () {
+    $($('ul').get().reverse()).each(function () {
+        const parentText = $(this).parent().find(">.tag>a").text();
+        $(this).find(">li").each(function () {
+            const aTag = $(this).find(">.tag>a");
+            if (parentText !== "") {
+                aTag.html(aTag.html().replace(parentText + "/", ""))
+            }
+        });
+    })
+});
