7967292db968dd5ae7b6da3c307779049dc8ff8a
AkrISrn <shairkarine@gmail.com>
2019-01-13 16:49:52 +0800

    fix

--- static/js/pace.on.js
+++ static/js/pace.on.js
@@ -2,19 +2,19 @@ Pace.on("done", function () {
     document.getElementById("content").style.opacity = "1";
     $("iframe.steam-widget").each(function () {
         this.setAttribute("src", `https:\/\/store.steampowered.com/widget/${this.getAttribute("data-id")}/`);
-        this.removeAttribute("data-id");
-        this.setAttribute("frameborder", "0");
         this.style.width = "100%";
         this.style.height = "200px";
     });
     $("iframe.kindle-widget").each(function () {
         this.setAttribute("src", `https:\/\/read.amazon.cn/kp/card?asin=${this.getAttribute("data-id")}&preview=inline`);
-        this.removeAttribute("data-id");
-        this.setAttribute("frameborder", "0");
         this.setAttribute("allowfullscreen", "");
-        this.style.maxWidth = "100%";
         this.style.width = "336px";
         this.style.height = "550px";
+    });
+    $("iframe.steam-widget, iframe.kindle-widget").each(function () {
+        this.removeAttribute("data-id");
+        this.setAttribute("frameborder", "0");
+        this.style.maxWidth = "100%";
     });
     $("img").each(function () {
         const imgSrc = this.getAttribute("data-src");
