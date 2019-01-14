d93c59dece5e6db492f3a2791543e9a5df485abe
AkrISrn <shairkarine@gmail.com>
2019-01-14 22:01:29 +0800

    fix tags page

--- static/js/tags.js
+++ static/js/tags.js
@@ -1,11 +1,18 @@
 $(function () {
-    $($('ul').get().reverse()).each(function () {
-        const parentText = $(this).parent().find(">.tag>a").text();
-        $(this).find(">li").each(function () {
+    $($('details').get().reverse()).each(function () {
+        const parentText = $(this).parent().find(">summary>.tag>a").text();
+        $(this).find(">summary").each(function () {
             const aTag = $(this).find(">.tag>a");
             if (parentText !== "") {
                 aTag.html(aTag.html().replace(new RegExp(`</i>${parentText}/`), "</i>"))
             }
         });
+        const summary = this.children[0];
+        summary.style.paddingBottom = "6px";
+        if (this.childElementCount === 1) {
+            this.classList.add("readonly");
+            summary.style.pointerEvents = "auto";
+            summary.style.cursor = "default";
+        }
     })
 });
