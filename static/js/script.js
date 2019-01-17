eaccaf25826ac5f00225f4d0db8d32e6a85f0c65
AkrISrn <shairkarine@gmail.com>
2019-01-17 12:49:33 +0800

    fix

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,21 @@
+let keyInput = "";
+const bindInput = (binds) => {
+    $(window).bind("keydown", (event) => {
+        keyInput += event.key;
+        for (let key in binds) {
+            if (keyInput.endsWith(key)) {
+                binds[key]();
+                break;
+            }
+        }
+    });
+};
+
 $(function () {
     $("a").each((index, element) => {
         if (element.getAttribute("href").startsWith("http")) {
             element.setAttribute("target", "_blank")
         }
     });
-
-    let keyInput = "";
-    $(window).bind("keydown", (event) => {
-        keyInput += event.key;
-        for (let key in inputBind["common"]) {
-            if (keyInput.endsWith(key)) {
-                inputBind["common"][key]();
-                break;
-            }
-        }
-    });
+    bindInput(inputBinds["common"])
 });
