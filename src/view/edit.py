e375edf1b79c4789d8c010f26fac698d5c51e067
AkrISrn <shairkarine@gmail.com>
2019-01-25 12:50:06 +0800

    fix

--- src/view/edit.py
+++ src/view/edit.py
@@ -5,7 +5,7 @@ from flask import Blueprint, abort, request, jsonify, url_for, redirect, render_
 
 from ..cache import get_file_cache
 from ..const import flag_notags, flag_highlight, flag_top, flag_fixed, flag_unignore, flag_ignore, flag_tag, \
-    flag_date, index_url_key
+    flag_date, index_url_key, articles_url_name
 from ..flag import get_flag_regexp
 from ..index import reindex, get_item_by_path
 from ..util import update_config_ignore_file_list, get_articles_dir_abspath
@@ -15,7 +15,7 @@ edit = Blueprint("edit", __name__)
 
 def toggle_flag(item_path, flag, is_on, data=None, force_reindex=False, force_update=False, check_index=True):
     def toggle():
-        if check_index and not get_item_by_path(item_path):
+        if check_index and not get_item_by_path(item_path) if item_path else None:
             abort(404)
         item_abspath = os.path.join(get_articles_dir_abspath(), item_path)
         if not os.path.exists(item_abspath):
@@ -51,72 +51,86 @@ def toggle_flag(item_path, flag, is_on, data=None, force_reindex=False, force_up
     return is_reindex
 
 
-@edit.route('/%s/<path:item_path>' % flag_ignore)
-def ignore(item_path, is_ignore=True):
-    # 加入忽略列表
-    update_config_ignore_file_list(item_path, is_ignore)
-    toggle_flag(item_path, flag_unignore if is_ignore else flag_ignore, False, None, True, False, False)
+@edit.route('/%s' % flag_ignore)
+def ignore(item_path=None, is_ignore=True):
+    if not item_path:
+        item_path = request.args.get("item_path")
+    if item_path:
+        # 加入忽略列表
+        update_config_ignore_file_list(item_path, is_ignore)
+        toggle_flag(item_path, flag_unignore if is_ignore else flag_ignore, False, None, True, False, False)
     return redirect(url_for('main.index_page'))
 
 
-@edit.route('/%s/<path:item_path>' % flag_unignore)
-def unignore(item_path):
-    return ignore(item_path, False)
+@edit.route('/%s' % flag_unignore)
+def unignore():
+    return ignore(request.args.get("item_path"), False)
 
 
-@edit.route('/%s/<path:item_path>' % flag_fixed)
-def fixed(item_path, is_fixed=True):
+@edit.route('/%s' % flag_fixed)
+def fixed(item_path=None, is_fixed=True):
+    if not item_path:
+        item_path = request.args.get("item_path")
     return jsonify(toggle_flag(item_path, flag_fixed, is_fixed))
 
 
-@edit.route('/un%s/<path:item_path>' % flag_fixed)
-def unfixed(item_path):
-    return fixed(item_path, False)
+@edit.route('/un%s' % flag_fixed)
+def unfixed():
+    return fixed(request.args.get("item_path"), False)
 
 
-@edit.route('/%s/<path:item_path>' % flag_top)
-def top(item_path, is_top=True):
+@edit.route('/%s' % flag_top)
+def top(item_path=None, is_top=True):
+    if not item_path:
+        item_path = request.args.get("item_path")
     return jsonify(toggle_flag(item_path, flag_top, is_top))
 
 
-@edit.route('/un%s/<path:item_path>' % flag_top)
-def untop(item_path):
-    return top(item_path, False)
+@edit.route('/un%s' % flag_top)
+def untop():
+    return top(request.args.get("item_path"), False)
 
 
-@edit.route('/%s/<path:item_path>' % flag_highlight)
-def highlight(item_path, is_highlight=True):
+@edit.route('/%s' % flag_highlight)
+def highlight(item_path=None, is_highlight=True):
+    if not item_path:
+        item_path = request.args.get("item_path")
     return jsonify(toggle_flag(item_path, flag_highlight, is_highlight))
 
 
-@edit.route('/un%s/<path:item_path>' % flag_highlight)
-def unhighlight(item_path):
-    return highlight(item_path, False)
+@edit.route('/un%s' % flag_highlight)
+def unhighlight():
+    return highlight(request.args.get("item_path"), False)
 
 
-@edit.route('/%s/<path:item_path>' % flag_notags)
-def notags(item_path, is_notags=True):
+@edit.route('/%s' % flag_notags)
+def notags(item_path=None, is_notags=True):
+    if not item_path:
+        item_path = request.args.get("item_path")
     return jsonify(toggle_flag(item_path, flag_notags, is_notags))
 
 
-@edit.route('/un%s/<path:item_path>' % flag_notags)
-def unnotags(item_path):
-    return notags(item_path, False)
+@edit.route('/un%s' % flag_notags)
+def unnotags():
+    return notags(request.args.get("item_path"), False)
 
 
-@edit.route('/%s/<path:item_path>' % flag_tag)
-def tag(item_path):
-    return jsonify(toggle_flag(item_path, flag_tag, False, request.args.get("data", ""), False, True))
+@edit.route('/%s' % flag_tag)
+def tag():
+    return jsonify(toggle_flag(request.args.get("item_path"),
+                               flag_tag, False, request.args.get("data", ""), False, True))
 
 
-@edit.route('/%s/<path:item_path>' % flag_date)
-def date(item_path):
-    return jsonify(toggle_flag(item_path, flag_date, False, request.args.get("data", ""), False, True))
+@edit.route('/%s' % flag_date)
+def date():
+    return jsonify(toggle_flag(request.args.get("item_path"),
+                               flag_date, False, request.args.get("data", ""), False, True))
 
 
-@edit.route('/article/<path:item_path>', methods=["GET", "POST"])
-def article(item_path):
-    item = get_item_by_path(item_path)
+@edit.route('/%s' % articles_url_name, methods=["GET", "POST"])
+def article():
+    item_path = request.args.get("item_path")
+    item = get_item_by_path(item_path) if item_path else None
     if not item:
         abort(404)
     item_abspath = os.path.join(get_articles_dir_abspath(), item_path)
