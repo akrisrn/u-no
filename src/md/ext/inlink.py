5ed73b1ce15a7c33b77bce3af93af4bf15112b10
AkrISrn <shairkarine@gmail.com>
2019-01-27 18:30:12 +0800

    fix md.ext.inlink

--- src/md/ext/inlink.py
+++ src/md/ext/inlink.py
@@ -13,12 +13,16 @@ class InlinkPreprocessor(Preprocessor):
         new_lines = []
         for line in lines:
             new_line = line
-            for group in re.finditer(r"\[(.*?)\]\((.*?)\)\+", line):
+            for group in re.finditer(r"\[(.*?)\]\((.*?)\)(\++)", line):
                 file_path = group.group(2).replace("../", "")
                 # 根据文件相对路径从索引文件中取出url
                 item = src.index.get_item_by_path(file_path)
                 if item:
-                    new_line = line.replace(group.group(), "[%s](%s)" % (group.group(1), item[index_url_key]))
+                    if len(group.group(3)) == 2:
+                        replace_value = item[index_url_key]
+                    else:
+                        replace_value = "[%s](%s)" % (group.group(1), item[index_url_key])
+                    new_line = line.replace(group.group(), replace_value)
             new_lines.append(new_line)
         return new_lines
 
@@ -26,7 +30,7 @@ class InlinkPreprocessor(Preprocessor):
 # 匹配[]()+语法为站内链接，小括号里填入文件相对路径，查找替换为索引文件中对应的url
 class InlinkExtension(Extension):
     def extendMarkdown(self, md):
-        md.preprocessors.register(InlinkPreprocessor(md), "inlink", 0)
+        md.preprocessors.register(InlinkPreprocessor(md), "inlink", 31)
 
 
 def makeExtension(**kwargs):
