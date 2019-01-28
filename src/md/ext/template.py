fce89f45054c24c65a67c13416f1ef87448e77a3
AkrISrn <shairkarine@gmail.com>
2019-01-28 18:34:22 +0800

    add param support to template syntax

--- src/md/ext/template.py
+++ src/md/ext/template.py
@@ -13,13 +13,39 @@ class TemplatePreprocessor(Preprocessor):
         self.seen = []
         super().__init__(md)
 
+    @staticmethod
+    def fill_params(param_dict, text):
+        for match in re.finditer(r'{{(\d+)(\|(.*?))?}}', text):
+            param_index = int(match.group(1))
+            if param_index in param_dict:
+                replace_value = param_dict[param_index]
+            else:
+                replace_value = match.group(3)
+            if replace_value is not None:
+                text = text.replace(match.group(), replace_value)
+        return text
+
     def parse_template(self, lines, file_name=None):
         new_lines = []
         for line in lines:
             new_line = line
-            match = re.match(r"^{%\s*(.*?)\s*%}$", new_line)
-            if match:
-                file_path = clean_link(match.group(1))
+            template_match = re.match(r"^{%\s*(.*?)\s*%}$", new_line)
+            if template_match:
+                params = template_match.group(1).split("|")[1:]
+                param_dict = {}
+                i = 0
+                for param in params:
+                    param_match = re.match(r'(\d+)=(.*)', param)
+                    if param_match:
+                        param_index = int(param_match.group(1))
+                        param_value = param_match.group(2)
+                    else:
+                        i += 1
+                        param_index = i
+                        param_value = param
+                    param_dict[param_index] = param_value
+
+                file_path = clean_link(template_match.group(1))
                 file_abspath = os.path.join(self.base_path, file_path)
                 if os.path.exists(file_abspath):
                     if file_path in self.seen:
@@ -27,7 +53,8 @@ class TemplatePreprocessor(Preprocessor):
                     if file_name:
                         self.seen.append(file_name)
                     with open(file_abspath, 'r', encoding="utf-8") as f:
-                        new_lines.extend([l2 for l2 in self.parse_template([l.rstrip('\r\n') for l in f], file_path)])
+                        new_lines.extend([self.fill_params(param_dict, l2) for l2 in
+                                          self.parse_template([l.rstrip('\r\n') for l in f], file_path)])
                     if file_name:
                         self.seen.remove(file_name)
                     continue
