a197de5c287d4f333a3aad39b0b8e13d5c42c698
AkrISrn <shairkarine@gmail.com>
2019-02-13 14:11:50 +0800

    fix

--- static/js/article.js
+++ static/js/article.js
@@ -110,6 +110,6 @@ $(function () {
     });
 
     $(".markdown-body").find("h1,h2,h3,h4,h5.h6").each(function () {
-        $(this).html($("<a style='color: #24292e'>").attr("href", "#" + $(this).attr("id")).html($(this).html()));
+        $(this).html($("<a style='color: #4a4a4a'>").attr("href", "#" + $(this).attr("id")).html($(this).html()));
     });
 });
